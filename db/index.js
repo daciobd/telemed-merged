08f500b12ffcf8d8f6c71645a02d3daf060e4b16
daciobd <daciobd@gmail.com>
2025-12-01 18:55:55 +0000

    Ensure db/index.js exports named db for ESM imports

--- db/index.js
+++ db/index.js
@@ -1,1 +1,13 @@
-module.exports = require('./index.cjs');
+// Shim para compatibilizar CommonJS (index.cjs) com import ESM { db } from '.../db/index.js'
+const dbModule = require('./index.cjs');
+
+// Tenta achar o objeto de conexão em dbModule.db, dbModule.default ou no próprio módulo.
+const db =
+  dbModule.db ||
+  dbModule.default ||
+  dbModule;
+
+module.exports = {
+  ...dbModule,
+  db,
+};
